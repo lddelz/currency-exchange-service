77126c7b9e27857484470a45f3fe1b1261af66a1
lukasz.danelczyk <lukasz.danelczyk.sp@lhsystems.com>
2022-05-18 18:03:53 +0200

    Init

---
+++ src/main/java/com/lddelz/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -0,0 +1,41 @@
+package com.lddelz.currencyexchangeservice.controller;
+
+import com.lddelz.currencyexchangeservice.model.CurrencyExchange;
+import com.lddelz.currencyexchangeservice.repository.CurrencyExchangeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
+
+@RestController
+public class CurrencyExchangeController {
+
+    private final Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
+
+    private final CurrencyExchangeRepository currencyExchangeRepository;
+    private final Environment environment;
+
+    public CurrencyExchangeController(CurrencyExchangeRepository currencyExchangeRepository, Environment environment) {
+        this.currencyExchangeRepository = currencyExchangeRepository;
+        this.environment = environment;
+    }
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) throws UnknownHostException {
+        CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
+
+        if (currencyExchange == null) {
+            throw new RuntimeException("Unable to Find data for " + from + " to " + to);
+        }
+        logger.info("Currency exchange service host: {}", Inet4Address.getLocalHost().getHostAddress());
+        currencyExchange.setPort(environment.getProperty("local.server.port"));
+        return currencyExchange;
+    }
+
+}
---
+++ src/main/java/com/lddelz/currencyexchangeservice/repository/CurrencyExchangeRepository.java
@@ -0,0 +1,10 @@
+package com.lddelz.currencyexchangeservice.repository;
+
+import com.lddelz.currencyexchangeservice.model.CurrencyExchange;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange, Long> {
+
+    CurrencyExchange findByFromAndTo(String from, String to);
+
+}
